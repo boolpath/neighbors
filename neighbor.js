1a1da72d194890fa722088bc80a8732e8e82bad9
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-05-03 00:45:39 -0500

    Initial commit

---
+++ neighbor.js
@@ -0,0 +1,28 @@
+/** MODULE INTERFACE
+ *@method {function} - 
+ */
+module.exports = {
+    createNeighbor: createNeighbor,
+    createNeighborhood: createNeighborhood
+};
+
+/*----------------------------------------------------------------------------*/
+
+/** 
+ * @param
+ * @returns
+ */
+function createNeighbor() {
+    
+}
+
+/** 
+ * @param {number} dimensions - The number of dimensions of the neighborhood model
+ * @param {object} coordinates - An array containing the coordinate axes of the multidimensional space
+ * @returns {object} neighborhood - 
+ */
+function createNeighborhood() {
+    var neighborhood = {};
+
+    return neighborhood;
+}
