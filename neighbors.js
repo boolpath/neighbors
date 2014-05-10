15f90535bf6713dcdf3145eec8f92670cc7f6cb5
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-05-09 19:42:14 -0500

    Removed the 'dimensions' parameter and generated the coordinate axes

--- neighbors.js
+++ neighbors.js
@@ -2,27 +2,51 @@
  *@method {function} - 
  */
 module.exports = {
-    createNeighbor: createNeighbor,
-    createNeighborhood: createNeighborhood
+    create: {
+        neighborhood: createNeighborhood 
+    }
 };
 
 /*----------------------------------------------------------------------------*/
 
 /** 
- * @param
- * @returns
- */
-function createNeighbor() {
-    
-}
-
-/** 
- * @param {number} dimensions - The number of dimensions of the neighborhood model
- * @param {object} coordinates - An array containing the coordinate axes of the multidimensional space
+ * @param {object} axes - An array containing the coordinate axes of the multidimensional neighborhood model
  * @returns {object} neighborhood - 
  */
-function createNeighborhood() {
-    var neighborhood = {};
+function createNeighborhood(axes) {
+    var neighborhood = {
+        axes: {}
+    };
 
-    return neighborhood;
+    // Parameter validation
+    if (!(axes instanceof Array)) {
+        return neighborhood;
+    }
+
+    // Multidimensional space generation
+    var dimensions = 0;
+    for (var index in axes) {
+        var axis = axes[index];
+        if (typeof axis === 'string') {
+            dimensions++;
+            Object.defineProperty(neighborhood.axes, axis, {
+                enumerable: true,
+                value: Object.create({}, {
+                    lower: {
+                        writable: true,
+                        enumerable: true,
+                        value: undefined
+                    },
+                    higher: {
+                        writable: true,
+                        enumerable: true,
+                        value: undefined
+                    }
+                })
+            });
+        }
+    }
+    neighborhood.dimension = dimensions;
+
+  return neighborhood;
 }
